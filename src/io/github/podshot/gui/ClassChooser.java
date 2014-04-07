75e63d792949daf1f91ac2c27cced24ca149c6dd
Podshot <ben.gothard3@gmail.com>
2014-04-07 19:53:42 -0400

    Added Class Chooser

--- src/io/github/podshot/gui/ClassChooser.java
+++ src/io/github/podshot/gui/ClassChooser.java
@@ -1,5 +1,21 @@
 package io.github.podshot.gui;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 public class ClassChooser {
+	
+	public Inventory getClassChooserGui() {
+		Inventory classChooser = Bukkit.createInventory(null, 9, ChatColor.DARK_RED + "Choose a Fighting Class");
+		
+		ItemStack soldier = new ItemStack(Material.IRON_HELMET);
+		ItemMeta imSoldier = soldier.getItemMeta();
+		imSoldier.setDisplayName(ChatColor.GRAY + "Soldier");
+		return classChooser;
+	}
 
 }
