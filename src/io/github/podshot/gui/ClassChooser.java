9e668742b3d59a14e70b08f169545d1840e670d5
Podshot <ben.gothard3@gmail.com>
2014-04-07 20:14:55 -0400

    Added Soldier Class

--- src/io/github/podshot/gui/ClassChooser.java
+++ src/io/github/podshot/gui/ClassChooser.java
@@ -1,5 +1,8 @@
 package io.github.podshot.gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -12,9 +15,17 @@ public class ClassChooser {
 	public Inventory getClassChooserGui() {
 		Inventory classChooser = Bukkit.createInventory(null, 9, ChatColor.DARK_RED + "Choose a Fighting Class");
 		
-		ItemStack soldier = new ItemStack(Material.IRON_HELMET);
+		ItemStack soldier = new ItemStack(Material.LEATHER_HELMET);
 		ItemMeta imSoldier = soldier.getItemMeta();
 		imSoldier.setDisplayName(ChatColor.GRAY + "Soldier");
+		List<String> soldierList = new ArrayList<String>();
+		soldierList.add(ChatColor.YELLOW + "Armed with a Standard Issue Rifle,");
+		soldierList.add(ChatColor.YELLOW + "partially used Armor, and a hand grenade.");
+		soldierList.add("");
+		soldierList.add(ChatColor.YELLOW + "This class makes up the backbone of any strong force");
+		imSoldier.setLore(soldierList);
+		soldier.setItemMeta(imSoldier);
+		classChooser.setItem(0, soldier);
 		return classChooser;
 	}
 
