77662c17cdaeb9646d04c62e9a0e23c25a93572a
Podshot <ben.gothard3@gmail.com>
2014-04-27 12:17:29 -0400

    Fixed Indentation

--- src/io/github/podshot/events/guns/Rifle.java
+++ src/io/github/podshot/events/guns/Rifle.java
@@ -15,35 +15,42 @@ public class Rifle implements Listener {
 
 	@EventHandler
 	public void onFireRifle(PlayerInteractEvent e) {
-		if (Internals.isWarDeclared()) {
-			if (e.getAction() == Action.RIGHT_CLICK_AIR) {
-				ItemStack gunIS = e.getItem();
-				if (gunIS.getType() == Material.MONSTER_EGG && gunIS.getDurability() == 50) {
-					String gun = gunIS.getItemMeta().getDisplayName().toString();
-					if (gun.equals("Standard Issue Rifle")) {
-						ItemProjectile bullet = new ItemProjectile("bullet-rifle", e.getPlayer(), new ItemStack(Material.STONE_BUTTON), 2.0F);
-						bullet.setIgnoreSomeBlocks(true);
-						bullet.boundingBox.shrink(2D, 2D, 2D);
-						int lvl = e.getPlayer().getLevel() - 1;
-						e.getPlayer().setLevel(lvl);
-						e.setCancelled(true);
-					}
-				}
+		if (!(Internals.isWarDeclared())) {
+			return;
+		}
+
+		if (!(e.getAction() == Action.RIGHT_CLICK_AIR)) {
+			return;
+		}
+
+		ItemStack gunIS = e.getItem();
+		if (gunIS.getType() == Material.MONSTER_EGG && gunIS.getDurability() == 50) {
+			String gun = gunIS.getItemMeta().getDisplayName().toString();
+			if (gun.equals("Standard Issue Rifle")) {
+				ItemProjectile bullet = new ItemProjectile("bullet-rifle", e.getPlayer(), new ItemStack(Material.STONE_BUTTON), 2.0F);
+				bullet.setIgnoreSomeBlocks(true);
+				bullet.boundingBox.shrink(2D, 2D, 2D);
+				int lvl = e.getPlayer().getLevel() - 1;
+				e.getPlayer().setLevel(lvl);
+				e.setCancelled(true);
 			}
 		}
 		return;
 	}
-	
+
 	@EventHandler
 	public void onRifleReload(PlayerInteractEvent e) {
-		if (Internals.isWarDeclared()) {
-			if (e.getAction() == Action.LEFT_CLICK_AIR) {
-				ItemStack gunIS = e.getItem();
-				if (gunIS.getType().equals(Material.MONSTER_EGG) && gunIS.getDurability() == 50) {
-					if (gunIS.getItemMeta().getDisplayName().toString().equals("Standard Issue Rifle")) {
-						e.getPlayer().setLevel(25);
-					}
-				}
+		if (!(Internals.isWarDeclared())) {
+			return;
+		}
+
+		if (!(e.getAction() == Action.LEFT_CLICK_AIR)) {
+			return;
+		}
+		ItemStack gunIS = e.getItem();
+		if (gunIS.getType().equals(Material.MONSTER_EGG) && gunIS.getDurability() == 50) {
+			if (gunIS.getItemMeta().getDisplayName().toString().equals("Standard Issue Rifle")) {
+				e.getPlayer().setLevel(25);
 			}
 		}
 	}
