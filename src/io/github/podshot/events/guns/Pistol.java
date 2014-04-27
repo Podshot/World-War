ac83d76177f2612e0e290be98fbf8ebbb157e223
Podshot <ben.gothard3@gmail.com>
2014-04-27 12:41:09 -0400

    Added beginnings of Pistol

---
+++ src/io/github/podshot/events/guns/Pistol.java
@@ -0,0 +1,23 @@
+package io.github.podshot.events.guns;
+
+import io.github.podshot.api.Gun;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class Pistol implements Gun, Listener {
+
+	@EventHandler
+	public void onFireGun(PlayerInteractEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@EventHandler
+	public void onGunReload(PlayerInteractEvent e) {
+		// TODO Auto-generated method stub
+	}
+	
+	
+
+}
