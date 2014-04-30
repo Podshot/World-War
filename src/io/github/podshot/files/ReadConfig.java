f65b1012e0e59bd4df7e257052506d88a0341c42
Podshot <ben.gothard3@gmail.com>
2014-04-30 08:23:19 -0400

    Added another config option

--- src/io/github/podshot/files/ReadConfig.java
+++ src/io/github/podshot/files/ReadConfig.java
@@ -6,12 +6,16 @@ import io.github.podshot.internals.ConfigInternals;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class ReadConfig {
+	
 	private WorldWar plugin = WorldWar.getInstance();
 
 	public void readConfig() {
 		FileConfiguration config = plugin.getConfig();
-		ConfigInternals.aatoe = config.getBoolean("Allow-Anyone-To-Enter-Vehicles");
-		ConfigInternals.huolagw = config.getBoolean("Hyped-Up-On-Lemonade-And-Gummy-Worms");
+		ConfigInternals.setAATOE(config.getBoolean("Allow-Anyone-To-Enter-Vehicles"));
+		ConfigInternals.setHUOLAQW(config.getBoolean("Hyped-Up-On-Lemonade-And-Gummy-Worms"));
+		ConfigInternals.setAnnounce(config.getBoolean("Announce-War-End-Cause-To-All-Players"));
+		//ConfigInternals.aatoe = config.getBoolean("Allow-Anyone-To-Enter-Vehicles");
+		//ConfigInternals.huolagw = config.getBoolean("Hyped-Up-On-Lemonade-And-Gummy-Worms");
 	}
 
 }
--- src/io/github/podshot/internals/ConfigInternals.java
+++ src/io/github/podshot/internals/ConfigInternals.java
@@ -2,7 +2,32 @@ package io.github.podshot.internals;
 
 public class ConfigInternals {
 
-	public static boolean aatoe;
-	public static boolean huolagw;
+	private static boolean aatoe;
+	private static boolean huolagw;
+	private static boolean announce;
+	
+	public static void setAATOE(boolean bool) {
+		aatoe = bool;
+	}
+	
+	public static boolean getAATOE() {
+		return aatoe;
+	}
+	
+	public static void setHUOLAQW(boolean bool) {
+		huolagw = bool;
+	}
+	
+	public static boolean getHUOLAQW() {
+		return huolagw;
+	}
+
+	public static void setAnnounce(boolean bool) {
+		announce = bool;		
+	}
+	
+	public static boolean getAnnounce() {
+		return announce;
+	}
 
 }
