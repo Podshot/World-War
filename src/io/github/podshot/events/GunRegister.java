fecb07db172b92bbdd1ea226ae8fa230289a0c79
Podshot <ben.gothard3@gmail.com>
2014-04-30 08:22:59 -0400

    Moved remote use to dedicated class

--- src/io/github/podshot/events/GunRegister.java
+++ src/io/github/podshot/events/GunRegister.java
@@ -2,6 +2,7 @@ package io.github.podshot.events;
 
 import io.github.podshot.WorldWar;
 import io.github.podshot.events.bullets.RifleBullet;
+import io.github.podshot.events.guns.Remote;
 import io.github.podshot.events.guns.Rifle;
 
 public class GunRegister {
@@ -10,6 +11,7 @@ public class GunRegister {
 		WorldWar plugin = WorldWar.getInstance();
 		plugin.getServer().getPluginManager().registerEvents(new Rifle(), plugin);
 		plugin.getServer().getPluginManager().registerEvents(new RifleBullet(), plugin);
+		plugin.getServer().getPluginManager().registerEvents(new Remote(), plugin);
 	}
 
 }
