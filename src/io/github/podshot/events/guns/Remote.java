73b25f2746ee8c5c55744ea841d6d5fa8d1341e3
Podshot <ben.gothard3@gmail.com>
2014-05-28 16:32:31 -0400

    Indentation

--- src/io/github/podshot/events/guns/Remote.java
+++ src/io/github/podshot/events/guns/Remote.java
@@ -9,6 +9,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class Remote implements Listener {
@@ -16,20 +17,24 @@ public class Remote implements Listener {
 	@EventHandler
 	public void onUseRemote(PlayerInteractEvent e) {
 		if (Internals.isWarDeclared()) {
-			if (e.getItem().getType().equals(Material.RECORD_11)) {
-				if (e.getItem().getItemMeta().getDisplayName().toString().equals("Remote")) {
-					List<String> coords = e.getItem().getItemMeta().getLore();
-					String x = coords.get(0).toString().split(": ")[1].toString();
-					String y = coords.get(1).toString().split(": ")[1].toString();
-					String z = coords.get(2).toString().split(": ")[1].toString();
-					String world = coords.get(3).toString().split(": ")[1].toString();
+			if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR) {
+				if (e.getItem().getType().equals(Material.RECORD_11)) {
+					if (e.getItem().getItemMeta().getDisplayName().toString().equals("Remote")) {
+						List<String> coords = e.getItem().getItemMeta().getLore();
+						String x = coords.get(0).toString().split(": ")[1].toString();
+						String y = coords.get(1).toString().split(": ")[1].toString();
+						String z = coords.get(2).toString().split(": ")[1].toString();
+						String world = coords.get(3).toString().split(": ")[1].toString();
+						e.getPlayer().getInventory().remove(e.getItem());
 
-					Location loc = new Location(Bukkit.getWorld(world), Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(z));
-					if (loc.getBlock().getType().equals(Material.SKULL)) {
-						if (!(Bukkit.getWorld(world).getChunkAt(loc).isLoaded())) {
-							Bukkit.getWorld(world).getChunkAt(loc).load(true);
+						Location loc = new Location(Bukkit.getWorld(world), Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(z));
+						if (loc.getBlock().getType().equals(Material.SKULL)) {
+							if (!(Bukkit.getWorld(world).getChunkAt(loc).isLoaded())) {
+								Bukkit.getWorld(world).getChunkAt(loc).load(true);
+							}
+							Bukkit.getWorld(world).createExplosion(loc, 3.5F);
 						}
-						Bukkit.getWorld(world).createExplosion(loc, 3.5F);
+						return;
 					}
 					return;
 				}
